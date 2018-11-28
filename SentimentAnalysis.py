fbdd117befe56bdfddce4f0ec99d72366cefca2b
Phil John <philjohn@eduroam-169-233-217-93.ucsc.edu>
2018-11-27 17:31:16 -0800

    SentimentAnlysis.py upload

---
+++ SentimentAnalysis.py
@@ -0,0 +1,37 @@
+#ML Asg 3
+import csv
+import math
+import operator
+import sys
+
+# Load and read our data set
+
+def loadDataset():
+ 	trainSet = []
+ 	train = open('train.csv')
+ 	trainData = list(csv.reader(train))
+
+ 	print(trainData[0])
+ 	
+
+ 	for i in range(1, len(trainData)):
+ 		trainSet.append(trainData[i])
+
+ 	return trainSet
+
+
+# create a dictionary such that key = word
+# and value is the amount of times the word is found
+
+
+def wordDictionary(trainSet):
+	for i in range(0,2):
+		instanceList = trainSet[i]
+		instanceWords = instanceList[2]
+		
+		
+
+print("hello")
+dataSet = loadDataset()
+
+wordDictionary(dataSet)
